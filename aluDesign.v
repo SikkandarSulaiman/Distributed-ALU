532a7d594f8c3ba6aa1a53bcd70f56d4edd27f60
Sikkandar Sulaiman <sikkandarsulaiman@gmail.com>
2016-10-22 22:32:43 +0530

    all modules (in Verilog) required for ALU design

--- aluDesign.v
+++ aluDesign.v
@@ -32,7 +32,7 @@ endmodule
 
 
 
-module m2to1(a,b,s,o);
+module mux2to1(a,b,s,o);
   input [7:0]a,b;
   input s;
   output reg [7:0]o;
@@ -60,7 +60,7 @@ module alu(a,b,s,clk,reset,d);
   wire [7:0]d1,d2,d3;
   au au1(a,b,{s[0],s[1]},d1);
   lu lu1(a,b,{s[2],s[3],s[4]},d2);
-  m2to1 m(d1,d2,s[5],d3);
+  mux2to1 m(d1,d2,s[5],d3);
   pipo p4(d3,d,clk,reset);
 endmodule
 
@@ -78,35 +78,9 @@ module alu4(a,b,clk,reset,s,S,d);
   alu alu2(a[1],b[1],s[1],clk,reset,d2);
   alu alu3(a[2],b[2],s[2],clk,reset,d3);
   alu alu4(a[3],b[3],s[3],clk,reset,d4);
-  m2to1 m1(d1,d2,S[1],o1);
-  m2to1 m2(d3,d4,S[1],o2);
-  m2to1 m3(o1,o2,S[0],d);
+  mux2to1 m1(d1,d2,S[1],o1);
+  mux2to1 m2(d3,d4,S[1],o2);
+  mux2to1 m3(o1,o2,S[0],d);
 endmodule
 
 
-
-module memory(in1,in2,op,d,clk,clr,out);
-  input [3:0]in1,in2;
-  input [5:0]op;
-  input [7:0]d;
-  input clk,clr;
-  reg [9:0][3:0][7:0]mem;
-  output reg [9:0][3:0][7:0]out;
-  int p,i,j,k; 
-  initial p=0;
-  always@(posedge clk)
-  begin
-    mem[p][0]=in1;
-    mem[p][1]=in2;
-    mem[p][2]=op;
-    mem[p][3]=d;
-    p=p+1;
-    if(clr)
-      for(i=0;i<=3;i=i+1)
-      for(j=0;j<=7;j=j+1)
-      for(k=0;k<=9;k=k+1)
-      mem[k][i][j]=0;
-    else
-      out=mem;
-    end
-endmodule
